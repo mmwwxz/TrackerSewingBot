c0070ccf3943bf817743dca54f11be25d9352d27
admin@MacBook-Pro-2.local <muhammad2808.a@mail.ru>
2023-11-15 17:36:04 +0600

    add new

--- handlers/markups.py
+++ handlers/markups.py
@@ -1,12 +1,18 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram import types
 
 # --- Main menu ---
 reports_btn = KeyboardButton('Отчет')
 consumption_btn = KeyboardButton('Расходы')
-filtration_btn = KeyboardButton('Фильтрация')
+filtration_btn = KeyboardButton('Поиск')
+
+# --- Filtration menu ---
+name_filtration_btn = InlineKeyboardButton('По имени')
+model_name_filtration_btn = InlineKeyboardButton('По названию модели')
 
 # --- Other menu ---
 help_btn = KeyboardButton('Отправить баг')
-mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(reports_btn, consumption_btn, filtration_btn,
-                                                         help_btn)
+mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(reports_btn, consumption_btn, filtration_btn, help_btn)
+
+filtrationMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(name_filtration_btn, model_name_filtration_btn)
+
