7ccbf354eb285ddf989be1bc6d4157d284212ca5
admin@MacBook-Pro-2.local <muhammad2808.a@mail.ru>
2023-11-17 19:10:19 +0600

    update filtr

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import logging
 import os
+# import io
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text, Command
 from aiogram.utils import executor
-from openpyxl import load_workbook
+from openpyxl import Workbook, load_workbook
 from datetime import datetime
+from aiogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 from handlers import markups as nav
 from config import class_for_main1 as nav2
@@ -23,6 +25,8 @@ if not os.path.exists('data/consumption.xlsx'):
     open('data/consumption.xlsx', 'w').close()
 
 dp = nav2.dp
+
+logging.basicConfig(level=logging.INFO)
 
 
 async def is_admin_user(message: types.Message):
@@ -80,54 +84,115 @@ async def process_unit_price(message: types.Message, state: FSMContext):
 @dp.message_handler(Text(equals="Поиск", ignore_case=True))
 async def search_options(message: types.Message):
     print("Получено сообщение 'Поиск'")
-    await message.reply("Выберите опцию поиска:", reply_markup=nav.filtrationMenu)
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(InlineKeyboardButton("По имени", callback_data="search_by_name"))
+    keyboard.add(InlineKeyboardButton("По названию модели", callback_data="search_by_model"))
+
+    await message.reply("Выберите опцию поиска:", reply_markup=keyboard)
 
 
-@dp.callback_query_handler(lambda c: c.data == "По имени")
-async def search_by_name(callback_query: types.CallbackQuery):
-    print("Получен запрос на поиск по имени")
-    await nav2.bot.send_message(callback_query.from_user.id, "Введите имя мастера для поиска:")
-    current_state = await nav2.dp.storage.get_state(chat=callback_query.message.chat.id,
-                                                    user=callback_query.from_user.id)
-    print("Текущее состояние:", current_state)
-    await nav2.FormSearch.name_db.set()
+@dp.callback_query_handler(lambda c: c.data.startswith("search"))
+async def search_option_callback(callback_query: CallbackQuery):
+    option = callback_query.data[len("search_"):]
+    if option == "by_name":
+        logging.info("Received request to search by name")
+        await nav2.bot.send_message(callback_query.from_user.id, "Введите имя мастера для поиска:")
+        current_state = await nav2.dp.storage.get_state(chat=callback_query.message.chat.id,
+                                                        user=callback_query.from_user.id)
+        logging.info("Current state: %s", current_state)
+        await nav2.FormSearch.name_db.set()
+    elif option == "by_model":
+        logging.info("Received request to search by model")
+        await nav2.bot.send_message(callback_query.from_user.id, "Введите название модели для поиска:")
+        current_state = await nav2.dp.storage.get_state(chat=callback_query.message.chat.id,
+                                                        user=callback_query.from_user.id)
+        logging.info("Current state: %s", current_state)
+        await nav2.FormSearch.model_db.set()
 
 
 @dp.message_handler(state=nav2.FormSearch.name_db)
 async def process_search_by_name(message: types.Message, state: FSMContext):
-    print("Получено сообщение для поиска по имени:", message.text)
+    logging.info("Received message for name search: %s", message.text)
     async with state.proxy() as data:
         master_name = message.text
 
-        results = nav2.session.query(nav2.Report).filter(nav2.Report.name == master_name).all()
+        try:
+            results = nav2.session.query(nav2.Report).filter(nav2.Report.name == master_name).all()
 
-        for result in results:
-            await message.reply(f"Мастер: {result.name}, Модель: {result.model_name}, Количество: {result.remaining}"
-                                f", Принято: {result.income}, Итог: {result.result_reports}")
+            if not results:
+                await message.reply(f"Нет результатов поиска для мастера '{master_name}'.")
+                return
+
+            # Загружаем существующую рабочую книгу или создаем новую, если она не существует
+            file_path = f"data/search_results_{master_name}.xlsx"
+            try:
+                workbook = load_workbook(file_path)
+                sheet = workbook.active
+            except FileNotFoundError:
+                workbook = Workbook()
+                sheet = workbook.active
+                sheet.append(["Мастер", "Модель", "Количество", "Принято", "Итог"])
+
+            # Записываем результаты поиска в файл Excel
+            for result in results:
+                sheet.append([result.name, result.model_name, result.remaining, result.income, result.result_reports])
+
+            # Сохраняем и закрываем файл
+            workbook.save(file_path)
+            workbook.close()
+
+            # Отправляем файл пользователю
+            with open(file_path, 'rb') as file:
+                caption = f"Результаты поиска по мастеру '{master_name}'"
+                await message.answer_document(file, caption=caption)
+
+        except Exception as e:
+            logging.error("Error querying the database: %s", str(e))
+            await message.reply("Произошла ошибка при выполнении запроса к базе данных.")
 
     await state.finish()
 
 
 # --------- SEARCH IN REPORTS BY MODEL NAME ---------
-
-@dp.callback_query_handler(lambda c: c.data == "По названию модели")
-async def search_by_model(callback_query: types.CallbackQuery):
-    print("Получен запрос на поиск по названию модели")
-    await nav2.bot.send_message(callback_query.from_user.id, "Введите название модели для поиска:")
-    await nav2.FormSearch.model_db.set()
-
-
 @dp.message_handler(state=nav2.FormSearch.model_db)
 async def process_search_by_model(message: types.Message, state: FSMContext):
-    print("Получено сообщение для поиска по названию модели:", message.text)
+    logging.info("Received message for model name search: %s", message.text)
     async with state.proxy() as data:
         model_name = message.text
 
-        results = nav2.session.query(nav2.Report).filter(nav2.Report.model_name == model_name).all()
+        try:
+            results = nav2.session.query(nav2.Report).filter(nav2.Report.model_name == model_name).all()
 
-        for result in results:
-            await message.reply(f"Мастер: {result.name}, Модель: {result.model_name}, Количество: {result.remaining}"
-                                f", Принято: {result.income}, Итог: {result.model_db}")
+            if not results:
+                await message.reply(f"Нет результатов поиска для модели '{model_name}'.")
+                return
+
+            # Загружаем существующую рабочую книгу или создаем новую, если она не существует
+            file_path = f"data/search_results_{model_name}.xlsx"
+            try:
+                workbook = load_workbook(file_path)
+                sheet = workbook.active
+            except FileNotFoundError:
+                workbook = Workbook()
+                sheet = workbook.active
+                sheet.append(["Мастер", "Модель", "Количество", "Принято", "Итог"])
+
+            # Записываем результаты поиска в файл Excel
+            for result in results:
+                sheet.append([result.name, result.model_name, result.remaining, result.income, result.result_reports])
+
+            # Сохраняем и закрываем файл
+            workbook.save(file_path)
+            workbook.close()
+
+            # Отправляем файл пользователю
+            with open(file_path, 'rb') as file:
+                caption = f"Результаты поиска по модели '{model_name}'"
+                await message.answer_document(file, caption=caption)
+
+        except Exception as e:
+            logging.error("Error querying the database: %s", str(e))
+            await message.reply("Произошла ошибка при выполнении запроса к базе данных.")
 
     await state.finish()
 
