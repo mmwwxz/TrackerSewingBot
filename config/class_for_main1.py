6c07fdc036a738dee4821393ffbfde79d1ee868b
admin@MacBook-Pro-2.local <muhammad2808.a@mail.ru>
2023-11-18 19:28:48 +0600

    update code

--- config/class_for_main1.py
+++ config/class_for_main1.py
@@ -9,7 +9,7 @@ import logging
 import os
 from aiogram import Dispatcher, Bot
 
-engine = create_engine('sqlite:///mydatabase.db', echo=True)
+engine = create_engine('sqlite:///config/mdatabase.db', echo=True)
 Base = declarative_base()
 
 # ---- BOT TOKEN ----
